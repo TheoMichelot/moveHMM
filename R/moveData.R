fb94ba0a7cde36f5652a09c7725f91f280df0741
TheoMichelot <theo.michelot@insa-rouen.fr>
2015-07-17 18:49:44 +0100

    Modif doc (include x/y) and check fields.

--- R/moveData.R
+++ R/moveData.R
@@ -1,13 +1,18 @@
 
 #' Constructor of moveData objects
 #'
-#' @param data A list containing the IDs (if any), step lengths, turning angles and covariates (if any).
+#' @param data A list containing the IDs (if any), step lengths, turning angles
+#' and covariates (if any), as well as the x/y values (either euclidean coordinates
+#' or longitude/latitude)
 #'
 #' @return An object moveData.
 #'
 #' @examples
 moveData <- function(data)
 {
+  if(is.null(data$step) | is.null(data$angle) | is.null(data$x) | is.null(data$y))
+    stop("Can't construct moveData object : fields are missing")
+
   obj <- data
 
   class(obj) <- append(class(obj),"moveData")
