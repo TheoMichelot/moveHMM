74632c989a9330a5df72c94d1360b8184a158d93
TheoMichelot <theo.michelot@insa-rouen.fr>
2015-07-20 17:19:31 +0100

    parDef function

---
+++ R/parDef.R
@@ -0,0 +1,60 @@
+
+#' Parameters definition
+#'
+#' @param stepDist Name of the distribution of the step length values.
+#' @param angleDist Name of the distribution of the turning angle values.
+#' @param nbStates Number of states of the HMM.
+#' @param estAngleMean TRUE if the mean of the turning angles distribution is estimated,
+#' FALSE otherwise.
+#'
+#' @return A list of two elements : parSize (= c(number of parameters of the step length
+#' distribution,number of parameters of the turning angle distribution)) and bounds
+#' (matrix with 2 columns and sum(parSize) rows. Each row contains the lower and upper
+#' bound for the correponding parameter).
+parDef <- function(stepDist=c("gamma","weibull","exp"),angleDist=c("vm","wrpcauchy"),nbStates,
+                   estAngleMean)
+{
+  stepFun <- match.arg(stepFun)
+  angleFun <- match.arg(angleFun)
+
+  parSize <- c(NA,NA)
+  switch(stepFun,
+         "gamma"={
+           parSize[1] <- 2
+           stepBounds <- matrix(c(0,Inf),ncol=2,nrow=2*nbStates,byrow=TRUE)
+         },
+         "weibull"={
+           parSize[1] <- 2
+           stepBounds <- matrix(c(0,Inf),ncol=2,nrow=2*nbStates,byrow=TRUE)
+         },
+         "exp"={
+           parSize[1] <- 1
+           stepBounds <- matrix(c(0,Inf),ncol=2,nrow=nbStates,byrow=TRUE)
+         })
+  switch(angleFun,
+         "vm"={
+           if(estAngleMean) {
+             parSize[2] <- 2
+             angleBounds <- matrix(c(rep(c(-pi,pi),nbStates),rep(c(0,Inf),nbStates)),
+                                   ncol=2,byrow=TRUE)
+           }
+           else {
+             parSize[2] <- 1
+             angleBounds <- matrix(c(0,Inf),ncol=2,nrow=nbStates,byrow=TRUE)
+           }
+         },
+         "wrpcauchy"={
+           if(estAngleMean) {
+             parSize[2] <- 2
+             angleBounds <- matrix(c(rep(c(-pi,pi),nbStates),rep(c(0,1),nbStates)),
+                                   ncol=2,byrow=TRUE)
+           }
+           else {
+             parSize[2] <- 1
+             angleBounds <- matrix(c(0,1),ncol=2,nrow=nbStates,byrow=TRUE)
+           }
+         })
+
+  bounds <- rbind(stepBounds,angleBounds)
+  return(list(parSize=parSize,bounds=bounds))
+}
