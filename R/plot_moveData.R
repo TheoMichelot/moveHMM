18b91c03ebeb629c848b4937675228bde8185fba
TheoMichelot <theo.michelot@insa-rouen.fr>
2015-07-23 18:04:35 +0100

    allow step only in simData and plot.moveData

--- R/plot_moveData.R
+++ R/plot_moveData.R
@@ -14,59 +14,73 @@ plot.moveData <- function(data,compact=FALSE)
 
   par(mar=c(5,4,4,2)-c(0,0,2,1)) # bottom, left, top, right
   par(ask=T)
-  if(!compact) {
+
+  if(is.null(data[[1]]$angle))
+  {
+    par(mfrow=c(1,2))
     for(i in 1:length(data)) {
-      par(mfrow=c(1,1))
-      plot(data[[i]]$x,data[[i]]$y,type="o",lwd=1.3,xlab="x",ylab="y",pch=20)
+      plot(data[[i]]$step,type="l",xlab="t",ylab="step length",
+           ylim=c(0,max(data[[i]]$step,na.rm=T)))
+      hist(data[[i]]$step,xlab="step length",main="")
       mtext(paste("Animal ID :",data[[i]]$ID),side=3,outer=TRUE,padj=2)
+    }
+  }
+  else
+  {
+    if(!compact) {
+      for(i in 1:length(data)) {
+        par(mfrow=c(1,1))
+        plot(data[[i]]$x,data[[i]]$y,type="o",lwd=1.3,xlab="x",ylab="y",pch=20)
+        mtext(paste("Animal ID :",data[[i]]$ID),side=3,outer=TRUE,padj=2)
+
+        par(mfrow=c(2,2))
+        plot(data[[i]]$step,type="l",xlab="t",ylab="step length",
+             ylim=c(0,max(data[[i]]$step,na.rm=T)))
+        plot(data[[i]]$angle,type="l",xlab="t",ylab="turning angle",
+             ylim=c(-pi,pi))
+        abline(h=c(-pi,0,pi),lty=2)
+
+        hist(data[[i]]$step,xlab="step length",main="")
+        hist(data[[i]]$angle,xlab="turning angle",main="")
+
+        mtext(paste("Animal ID :",data[[i]]$ID),side=3,outer=TRUE,padj=2)
+      }
+    }
+    else {
+      par(mfrow = c(1,1))
+      xmin <- Inf; xmax <- -Inf
+      ymin <- Inf; ymax <- -Inf
+      stepmax <- -Inf; nbObs <- -Inf
+      for(i in 1:length(data)) {
+        if(min(data[[i]]$x,na.rm=T)<xmin) xmin <- min(data[[i]]$x,na.rm=T) # na.rm=T to ignore the NAs
+        if(min(data[[i]]$y,na.rm=T)<ymin) ymin <- min(data[[i]]$y,na.rm=T)
+        if(max(data[[i]]$x,na.rm=T)>xmax) xmax <- max(data[[i]]$x,na.rm=T)
+        if(max(data[[i]]$y,na.rm=T)>ymax) ymax <- max(data[[i]]$y,na.rm=T)
+        if(max(data[[i]]$step,na.rm=T)>stepmax) stepmax <- max(data[[i]]$step,na.rm=T)
+        if(length(data[[i]]$x)>nbObs) nbObs <- length(data[[i]]$x)
+      }
+      plot(data[[1]]$x,data[[1]]$y,type="o",pch=20,lwd=1.3,col=1, cex=0.5,
+           xlim=c(xmin,xmax),ylim=c(ymin,ymax),xlab="x",ylab="y")
+      for(i in 2:length(data)) {
+        points(data[[i]]$x,data[[i]]$y,type="o",pch=20,lwd=1.3,col=i,cex=0.5)
+      }
 
       par(mfrow=c(2,2))
-      plot(data[[i]]$step,type="l",xlab="t",ylab="step length",
-           ylim=c(0,max(data[[i]]$step,na.rm=T)))
-      plot(data[[i]]$angle,type="l",xlab="t",ylab="turning angle",
-           ylim=c(-pi,pi))
-      abline(h=c(-pi,0,pi),lty=2)
+      for(i in 1:length(data)) {
+        plot(data[[i]]$step,type="l",xlab="t",ylab="step length",
+             ylim=c(0,max(data[[i]]$step,na.rm=T)))
+        plot(data[[i]]$angle,type="l",xlab="t",ylab="turning angle",
+             ylim=c(-pi,pi))
+        abline(h=c(-pi,0,pi),lty=2)
 
-      hist(data[[i]]$step,xlab="step length",main="")
-      hist(data[[i]]$angle,xlab="turning angle",main="")
+        hist(data[[i]]$step,xlab="step length",main="")
+        hist(data[[i]]$angle,xlab="turning angle",main="")
 
-      mtext(paste("Animal ID :",data[[i]]$ID),side=3,outer=TRUE,padj=2)
+        mtext(paste("Animal ID :",data[[i]]$ID),side=3,outer=TRUE,padj=2)
+      }
     }
   }
-  else {
-    par(mfrow = c(1,1))
-    xmin <- Inf; xmax <- -Inf
-    ymin <- Inf; ymax <- -Inf
-    stepmax <- -Inf; nbObs <- -Inf
-    for(i in 1:length(data)) {
-      if(min(data[[i]]$x,na.rm=T)<xmin) xmin <- min(data[[i]]$x,na.rm=T) # na.rm=T to ignore the NAs
-      if(min(data[[i]]$y,na.rm=T)<ymin) ymin <- min(data[[i]]$y,na.rm=T)
-      if(max(data[[i]]$x,na.rm=T)>xmax) xmax <- max(data[[i]]$x,na.rm=T)
-      if(max(data[[i]]$y,na.rm=T)>ymax) ymax <- max(data[[i]]$y,na.rm=T)
-      if(max(data[[i]]$step,na.rm=T)>stepmax) stepmax <- max(data[[i]]$step,na.rm=T)
-      if(length(data[[i]]$x)>nbObs) nbObs <- length(data[[i]]$x)
-    }
-    plot(data[[1]]$x,data[[1]]$y,type="o",pch=20,lwd=1.3,col=1, cex=0.5,
-         xlim=c(xmin,xmax),ylim=c(ymin,ymax),xlab="x",ylab="y")
-    for(i in 2:length(data)) {
-      points(data[[i]]$x,data[[i]]$y,type="o",pch=20,lwd=1.3,col=i,cex=0.5)
-    }
 
-    par(mfrow=c(2,2))
-    for(i in 1:length(data)) {
-      plot(data[[i]]$step,type="l",xlab="t",ylab="step length",
-           ylim=c(0,max(data[[i]]$step,na.rm=T)))
-      plot(data[[i]]$angle,type="l",xlab="t",ylab="turning angle",
-           ylim=c(-pi,pi))
-      abline(h=c(-pi,0,pi),lty=2)
-
-      hist(data[[i]]$step,xlab="step length",main="")
-      hist(data[[i]]$angle,xlab="turning angle",main="")
-
-      mtext(paste("Animal ID :",data[[i]]$ID),side=3,outer=TRUE,padj=2)
-    }
-
-  }
   par(mfrow=c(1,1))
   par(mar=c(5,4,4,2)) # back to default layout
 }
--- R/simData.R
+++ R/simData.R
@@ -5,12 +5,8 @@
 #' @param nbStates Number of behavioural states to simulate.
 #' @param stepDist Name of the distribution from which to draw the step length values.
 #' @param angleDist Name of the distribution from which to draw the turning angle values.
-#' @param stepPar Parameters of the step length distribution. Must be provided in a
-#' matrix with one row for each parameter (in the order expected by the pdf of stepDist),
-#' and one column for each state.
-#' @param anglePar Parameters of the turning angle distribution. Must be provided in a
-#' matrix with one row for each parameter (in the order expected by the pdf of angleDist),
-#' and one column for each state.
+#' @param stepPar Parameters of the step length distribution.
+#' @param anglePar Parameters of the turning angle distribution.
 #' @param nbCovs Number of covariates to simulate (0 by default).
 #' @param zeroInflation TRUE if the step length distribution is inflated in zero.
 #'
@@ -27,9 +23,14 @@
 #' stepDist <- "weibull"
 #' angleDist <- "wrpcauchy"
 #' data <- simData(5,2,stepDist,angleDist,stepPar,anglePar)
+#'
+#' # step length only :
+#' stepPar <- c(1,10,1,5,0.2,0.3) # mean1, mean2, sd1, sd2, z1, z2
+#' stepDist <- "gamma"
+#' data <- simData(5,2,stepDist,"NULL",stepPar,nbCovs=2,zeroInflation=TRUE)
 
 simData <- function(nbAnimals,nbStates,stepDist=c("gamma","weibull","exp"),
-                    angleDist=c("vm","wrpcauchy"),stepPar,anglePar,nbCovs=0,
+                    angleDist=c("NULL","vm","wrpcauchy"),stepPar,anglePar=NULL,nbCovs=0,
                     zeroInflation=FALSE)
 {
   # check arguments
@@ -41,13 +42,22 @@ simData <- function(nbAnimals,nbStates,stepDist=c("gamma","weibull","exp"),
   if(nbAnimals<1) stop("nbAnimals should be at least 1.")
   if(nbStates<1) stop("nbStates should be at least 1.")
   p <- parDef(stepDist,angleDist,nbStates,TRUE,zeroInflation)
+
+  print(stepPar)
+  print(anglePar)
+  print(p$parSize)
+
   if(length(stepPar)!=p$parSize[1]*nbStates | length(anglePar)!=p$parSize[2]*nbStates)
     stop("Wrong number of parameters")
   stepBounds <- p$bounds[1:(p$parSize[1]*nbStates),]
-  angleBounds <- p$bounds[(p$parSize[1]*nbStates+1):nrow(p$bounds),]
-  if(length(which(stepPar<stepBounds[,1] | stepPar>stepBounds[,2]))>0 |
-       length(which(anglePar<angleBounds[,1] | anglePar>angleBounds[,2]))>0)
-    stop("Check the parameters bounds.")
+  if(length(which(stepPar<stepBounds[,1] | stepPar>stepBounds[,2]))>0)
+    stop("Check the step length parameters bounds.")
+  if(angleDist!="NULL")
+  {
+    angleBounds <- p$bounds[(p$parSize[1]*nbStates+1):nrow(p$bounds),]
+    if(length(which(anglePar<angleBounds[,1] | anglePar>angleBounds[,2]))>0)
+      stop("Check the turning angle parameters bounds.")
+  }
 
   # generate regression parameters for transition probabilities
   beta <- matrix(rnorm((nbCovs+1)*nbStates*(nbStates-1)),nrow=nbCovs+1,ncol=nbStates*(nbStates-1))
@@ -65,7 +75,7 @@ simData <- function(nbAnimals,nbStates,stepDist=c("gamma","weibull","exp"),
     zeroMass <- rep(0,nbStates)
     stepPar <- par$stepPar
   }
-  anglePar <- par$anglePar
+  anglePar <- par$anglePar # i.e. NULL if angleDist=="NULL"
 
   data <- list()
   for(i in 1:nbAnimals) {
@@ -100,7 +110,8 @@ simData <- function(nbAnimals,nbStates,stepDist=c("gamma","weibull","exp"),
     # simulate movement path X
     X <- matrix(NA,nrow=nbObs,ncol=2)
     step <- rep(NA,nbObs)
-    angle <- rep(NA,nbObs)
+    if(angleDist!="NULL") angle <- rep(NA,nbObs)
+    else angle <- NULL
     X[1,] <- c(0,0) # initial position of the animal
     phi <- 0
     for(k in 1:(nbObs-1)) {
@@ -113,13 +124,15 @@ simData <- function(nbAnimals,nbStates,stepDist=c("gamma","weibull","exp"),
         for(j in 1:nrow(stepPar))
           stepArgs[[j+1]] <- stepPar[j,Z[k]]
       }
-      if(nrow(anglePar)==1) angleArgs[[2]] <- anglePar[Z[k]]
-      else {
-        for(j in 1:nrow(anglePar))
-          angleArgs[[j+1]] <- anglePar[j,Z[k]]
+      if(angleDist!="NULL") {
+        if(nrow(anglePar)==1) angleArgs[[2]] <- anglePar[Z[k]]
+        else {
+          for(j in 1:nrow(anglePar))
+            angleArgs[[j+1]] <- anglePar[j,Z[k]]
+        }
+        angle[k] <- do.call(angleFun,angleArgs)-pi # angle between -pi and pi
+        phi<-phi+angle[k]
       }
-      angle[k] <- do.call(angleFun,angleArgs)-pi # angle between -pi and pi
-      phi<-phi+angle[k]
 
       # conversion between mean/sd and shape/scale if necessary
       if(stepFun=="rweibull" | stepFun=="rgamma") {
@@ -138,7 +151,7 @@ simData <- function(nbAnimals,nbStates,stepDist=c("gamma","weibull","exp"),
       m <- step[k]*c(Re(exp(1i*phi)),Im(exp(1i*phi)))
       X[k+1,] <- X[k,] + m
     }
-    angle[1] <- NA # the first angle value is arbitrary
+    if(angleDist!="NULL") angle[1] <- NA # the first angle value is arbitrary
 
     data[[i]] <- list(ID=as.character(i),x=X[,1],y=X[,2],step=step,angle=angle,covs=covs)
   }
