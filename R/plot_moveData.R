7e8014ae0437094803a91be3e9e24844b291b043
TheoMichelot <theo.michelot@insa-rouen.fr>
2015-07-24 14:53:57 +0100

    switch plot.moveData to dataframe

--- R/plot_moveData.R
+++ R/plot_moveData.R
@@ -9,41 +9,51 @@ plot.moveData <- function(data,compact=FALSE)
 {
   # check arguments
   if(length(data)<1) stop("The data input is empty.")
-  if(is.null(data[[1]]$x) | is.null(data[[1]]$y) | is.null(data[[1]]$step))
+  if(is.null(data$x) | is.null(data$y) | is.null(data$step))
     stop("Missing field(s) in data.")
 
   par(mar=c(5,4,4,2)-c(0,0,2,1)) # bottom, left, top, right
-  par(ask=T)
+  par(ask=TRUE)
 
-  if(is.null(data[[1]]$angle)) # only step length is provided
+  nbAnimals <- length(unique(data$ID))
+
+  if(is.null(data$angle)) # only step length is provided
   {
     par(mfrow=c(1,2))
-    for(i in 1:length(data)) {
-      plot(data[[i]]$step,type="l",xlab="t",ylab="step length",
-           ylim=c(0,max(data[[i]]$step,na.rm=T)))
-      hist(data[[i]]$step,xlab="step length",main="")
-      mtext(paste("Animal ID :",data[[i]]$ID),side=3,outer=TRUE,padj=2)
+    for(zoo in 1:nbAnimals) {
+      ID <- unique(data$ID)[zoo]
+      step <- data$step[which(data$ID==ID)]
+      plot(step,type="l",xlab="t",ylab="step length",
+           ylim=c(0,max(step,na.rm=T)))
+      hist(step,xlab="step length",main="")
+      mtext(paste("Animal ID :",ID),side=3,outer=TRUE,padj=2)
     }
   }
   else # step length and turning angle are provided
   {
     if(!compact) {
-      for(i in 1:length(data)) {
+      for(zoo in 1:nbAnimals) {
+        ID <- unique(data$ID)[zoo]
+        x <- data$x[which(data$ID==ID)]
+        y <- data$y[which(data$ID==ID)]
+        step <- data$step[which(data$ID==ID)]
+        angle <- data$angle[which(data$ID==ID)]
+
         par(mfrow=c(1,1))
-        plot(data[[i]]$x,data[[i]]$y,type="o",lwd=1.3,xlab="x",ylab="y",pch=20)
-        mtext(paste("Animal ID :",data[[i]]$ID),side=3,outer=TRUE,padj=2)
+        plot(x,y,type="o",lwd=1.3,xlab="x",ylab="y",pch=20)
+        mtext(paste("Animal ID :",ID),side=3,outer=TRUE,padj=2)
 
         par(mfrow=c(2,2))
-        plot(data[[i]]$step,type="l",xlab="t",ylab="step length",
-             ylim=c(0,max(data[[i]]$step,na.rm=T)))
-        plot(data[[i]]$angle,type="l",xlab="t",ylab="turning angle",
+        plot(step,type="l",xlab="t",ylab="step length",
+             ylim=c(0,max(step,na.rm=T)))
+        plot(angle,type="l",xlab="t",ylab="turning angle",
              ylim=c(-pi,pi))
         abline(h=c(-pi,0,pi),lty=2)
 
-        hist(data[[i]]$step,xlab="step length",main="")
-        hist(data[[i]]$angle,xlab="turning angle",main="")
+        hist(step,xlab="step length",main="")
+        hist(angle,xlab="turning angle",main="")
 
-        mtext(paste("Animal ID :",data[[i]]$ID),side=3,outer=TRUE,padj=2)
+        mtext(paste("Animal ID :",ID),side=3,outer=TRUE,padj=2)
       }
     }
     else {
@@ -51,36 +61,54 @@ plot.moveData <- function(data,compact=FALSE)
       xmin <- Inf; xmax <- -Inf
       ymin <- Inf; ymax <- -Inf
       stepmax <- -Inf; nbObs <- -Inf
-      for(i in 1:length(data)) {
-        if(min(data[[i]]$x,na.rm=T)<xmin) xmin <- min(data[[i]]$x,na.rm=T) # na.rm=T to ignore the NAs
-        if(min(data[[i]]$y,na.rm=T)<ymin) ymin <- min(data[[i]]$y,na.rm=T)
-        if(max(data[[i]]$x,na.rm=T)>xmax) xmax <- max(data[[i]]$x,na.rm=T)
-        if(max(data[[i]]$y,na.rm=T)>ymax) ymax <- max(data[[i]]$y,na.rm=T)
-        if(max(data[[i]]$step,na.rm=T)>stepmax) stepmax <- max(data[[i]]$step,na.rm=T)
-        if(length(data[[i]]$x)>nbObs) nbObs <- length(data[[i]]$x)
+      for(zoo in 1:nbAnimals) {
+        ID <- unique(data$ID)[zoo]
+        x <- data$x[which(data$ID==ID)]
+        y <- data$y[which(data$ID==ID)]
+        step <- data$step[which(data$ID==ID)]
+
+        if(min(x,na.rm=T)<xmin) xmin <- min(x,na.rm=T) # na.rm=T to ignore the NAs
+        if(min(y,na.rm=T)<ymin) ymin <- min(y,na.rm=T)
+        if(max(x,na.rm=T)>xmax) xmax <- max(x,na.rm=T)
+        if(max(y,na.rm=T)>ymax) ymax <- max(y,na.rm=T)
+        if(max(step,na.rm=T)>stepmax) stepmax <- max(step,na.rm=T)
+        if(length(x)>nbObs) nbObs <- length(x)
       }
-      plot(data[[1]]$x,data[[1]]$y,type="o",pch=20,lwd=1.3,col=1, cex=0.5,
+
+      ID <- unique(data$ID)[1]
+      x <- data$x[which(data$ID==ID)]
+      y <- data$y[which(data$ID==ID)]
+      plot(x,y,type="o",pch=20,lwd=1.3,col=1, cex=0.5,
            xlim=c(xmin,xmax),ylim=c(ymin,ymax),xlab="x",ylab="y")
-      for(i in 2:length(data)) {
-        points(data[[i]]$x,data[[i]]$y,type="o",pch=20,lwd=1.3,col=i,cex=0.5)
+
+      for(zoo in 2:length(data)) {
+        ID <- unique(data$ID)[zoo]
+        x <- data$x[which(data$ID==ID)]
+        y <- data$y[which(data$ID==ID)]
+        points(x,y,type="o",pch=20,lwd=1.3,col=zoo,cex=0.5)
       }
 
       par(mfrow=c(2,2))
-      for(i in 1:length(data)) {
-        plot(data[[i]]$step,type="l",xlab="t",ylab="step length",
-             ylim=c(0,max(data[[i]]$step,na.rm=T)))
-        plot(data[[i]]$angle,type="l",xlab="t",ylab="turning angle",
+      for(zoo in 1:nbAnimals) {
+        ID <- unique(data$ID)[zoo]
+        step <- data$step[which(data$ID==ID)]
+        angle <- data$angle[which(data$ID==ID)]
+
+        plot(step,type="l",xlab="t",ylab="step length",
+             ylim=c(0,max(step,na.rm=T)))
+        plot(angle,type="l",xlab="t",ylab="turning angle",
              ylim=c(-pi,pi))
         abline(h=c(-pi,0,pi),lty=2)
 
-        hist(data[[i]]$step,xlab="step length",main="")
-        hist(data[[i]]$angle,xlab="turning angle",main="")
+        hist(step,xlab="step length",main="")
+        hist(angle,xlab="turning angle",main="")
 
-        mtext(paste("Animal ID :",data[[i]]$ID),side=3,outer=TRUE,padj=2)
+        mtext(paste("Animal ID :",ID),side=3,outer=TRUE,padj=2)
       }
     }
   }
 
+  par(ask=FALSE)
   par(mfrow=c(1,1))
   par(mar=c(5,4,4,2)) # back to default layout
 }
