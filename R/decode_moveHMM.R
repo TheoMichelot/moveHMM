66b0745c147eb8fc1c8363fd8c3d8b86bc956172
TheoMichelot <theo.michelot@gmail.com>
2015-08-10 20:01:47 +0100

    update decode function

--- R/decode_moveHMM.R
+++ R/decode_moveHMM.R
@@ -1,11 +1,22 @@
 
+decode <- function(m) UseMethod("decode") # define generic method decode
+
 #' Decode the model
 #'
 #' @param m moveHMM object
+#'
+#' @return List of : sp (state probabilities), ...
+#' @examples
+#' m <- example$mod # moveHMM object (returned by fitHMM)
+#'
+#' dec <- decode(m)
+
 decode.moveHMM <- function(m)
 {
-  # states probabilities, residuals...
+  # states probabilities
+  sp <- stateProbs(m)
 
-  d <- list()
+  #residuals...
+  d <- list(sp=sp)
   return(d)
 }
