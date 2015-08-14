b31bb1980ac6008887856f44ea7324fc214de9f8
TheoMichelot <theo.michelot@gmail.com>
2015-08-14 15:07:10 +0100

    replace cout by Rcout

--- src/nLogLike.cpp
+++ src/nLogLike.cpp
@@ -125,7 +125,7 @@ double nLogLike_rcpp(int nbStates, arma::mat beta, arma::mat covs, DataFrame dat
     }
 
     for(int i=0;i<30;i++)
-        cout << allProbs(i,0) << ", " << allProbs(i,1) << endl;
+        Rcout << allProbs(i,0) << ", " << allProbs(i,1) << endl;
 
     // 3. Forward algorithm
     arma::mat gamma(nbStates,nbStates);
