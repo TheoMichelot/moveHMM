584200f5e0eef7a88877fc421aab000c19a64bff
TheoMichelot <theo.michelot@gmail.com>
2015-08-19 11:57:48 +0100

    confidence intervals (confIntervals and deltaMethod)

---
+++ R/confIntervals_moveHMM.R
@@ -0,0 +1,60 @@
+
+#' Generic confIntervals method
+#' @param m Fitted model
+confIntervals <- function(m) UseMethod("confIntervals") # define generic method confIntervals
+
+#' Confidence intervals
+#'
+#' Computes the confidence intervals of the step length parameters, as well as for the transition
+#' probabilities regression parameters.
+#'
+#' @method confIntervals moveHMM
+#'
+#' @param m A moveHMM object
+#'
+#' @return inf (inferior bound of 95% confidence interval) and sup (superior bound of 95%
+#' confidence interval) for the parameters of the step lengths distribution, and for the
+#' transition probabilities regression parameters.
+#'
+#' @examples
+#' m <- ex$mod # moveHMM object, as returned by fitHMM
+#'
+#' confIntervals(m)
+
+confIntervals.moveHMM <- function(m)
+{
+  nbStates <- ncol(m$mle$stepPar)
+
+  # identify covariates
+  covsCol <- which(names(m$data)!="ID" & names(m$data)!="x" & names(m$data)!="y" &
+                     names(m$data)!="step" & names(m$data)!="angle")
+  nbCovs <- length(covsCol)-1 # substract intercept column
+
+  # inverse of Hessian
+  Sigma <- ginv(m$mod$hessian)
+  var <- diag(Sigma)
+
+  p <- parDef(m$stepDist,m$angleDist,nbStates,m$estAngleMean,m$zeroInflation)
+
+  # identify parameters of interest
+  i1 <- p$parSize[1]*nbStates
+  i2 <- sum(p$parSize)*nbStates+1
+  i3 <- i2+nbStates*(nbStates-1)*(nbCovs+1)-1
+
+  est <- c(m$mod$estimate[1:i1],m$mod$estimate[i2:i3])
+
+  var <- c(var[1:i1],var[i2:i3])
+
+  # compute inf and sup for working parameters
+  winf <- est-1.96*sqrt(var)
+  wsup <- est+1.96*sqrt(var)
+
+  # compute inf and sup on natural scale
+  inf <- w2n(winf,p$bounds[1:i1,],c(p$parSize[1],0),nbStates,nbCovs,FALSE,TRUE)
+  sup <- w2n(wsup,p$bounds[1:i1,],c(p$parSize[1],0),nbStates,nbCovs,FALSE,TRUE)
+
+  inf <- list(stepPar=inf$stepPar,beta=inf$beta)
+  sup <- list(stepPar=sup$stepPar,beta=sup$beta)
+
+  return(list(inf=inf,sup=sup))
+}
---
+++ R/deltaMethod_moveHMM.R
@@ -0,0 +1,59 @@
+
+#' Generic deltaMethod method
+#' @param m Fitted model
+deltaMethod <- function(m) UseMethod("deltaMethod") # define generic method deltaMethod
+
+#' Delta method
+#'
+#' Computes confidence intervals for the turning angle parameters, using the delta method.
+#'
+#' @method deltaMethod moveHMM
+#'
+#' @param m A moveHMM object.
+#'
+#' @return inf (inferior bound of 95% confidence interval) and sup (superior bound of 95%
+#' confidence interval) for the parameters of the turning angle distribution.
+#'
+#' @examples
+#' m <- ex$mod # moveHMM object, as returned by fitHMM
+#'
+#' deltaMethod(m)
+
+deltaMethod.moveHMM <- function(m)
+{
+    wpar <- m$mod$estimate
+    nbStates <- ncol(m$mle$stepPar)
+
+    # identify covariates
+    covsCol <- which(names(m$data)!="ID" & names(m$data)!="x" & names(m$data)!="y" &
+                       names(m$data)!="step" & names(m$data)!="angle")
+    nbCovs <- length(covsCol)-1 # substract intercept column
+
+    foo <- length(wpar)-nbStates+2
+    wpar <- wpar[-(foo:length(wpar))]
+    foo <- length(wpar)-(nbCovs+1)*nbStates*(nbStates-1)+1
+    wpar <- wpar[-(foo:length(wpar))]
+
+    # identify working parameters for the angle distribution (x and y)
+    foo <- length(wpar)-nbStates+1
+    x <- wpar[(foo-nbStates):(foo-1)]
+    y <- wpar[foo:length(wpar)]
+
+    # compute natural parameters for the angle distribution
+    angleMean <- Arg(x+1i*y)
+    kappa <- sqrt(x^2+y^2)
+
+    Sigma <- ginv(m$mod$hessian)
+    Sigma <- Sigma[(foo-nbStates):length(wpar),(foo-nbStates):length(wpar)]
+
+    grad_kappa <- c(x/sqrt(x^2+y^2),y/sqrt(x^2+y^2))
+    grad_angleMean <- c(-y/(x^2+y^2),1/(x^2+y^2))
+
+    var_kappa <- grad_kappa%*%Sigma%*%grad_kappa
+    var_angleMean <- grad_angleMean%*%Sigma%*%grad_angleMean
+
+    inf <- rbind(angleMean-1.96*sqrt(var_angleMean),kappa-1.96*sqrt(var_kappa))
+    sup <- rbind(angleMean+1.96*sqrt(var_angleMean),kappa+1.96*sqrt(var_kappa))
+
+    return(list(inf=inf,sup=sup))
+}
