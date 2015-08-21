916fa0be0344c8c230aab2db2f74badbda8c53e1
TheoMichelot <theo.michelot@gmail.com>
2015-08-21 17:19:51 +0100

    AIC for several models

--- R/AIC_moveHMM.R
+++ R/AIC_moveHMM.R
@@ -6,16 +6,36 @@
 #' @method AIC moveHMM
 #'
 #' @param object A moveHMM object.
-#' @param ... Currently unused. For compatibility with generic method.
+#' @param ... Optional additional moveHMM objects, to compare AICs of the different models.
 #' @param k Penalty per parameter. Default : 2 ; for classical AIC.
 #'
-#' @return The AIC of the fitted model.
+#' @return The AIC of the model(s) provided.
 
 AIC.moveHMM <- function(object,...,k=2)
 {
-  m <- object
-  nbPar <- length(m$mle$stepPar)+length(m$mle$anglePar)+length(m$mle$beta)+length(m$mle$delta)-1
-  maxLogLike <- -m$mod$minimum
-  AIC <- -2*maxLogLike+k*nbPar
-  return(AIC)
+  models <- list(...)
+
+  if(length(models)>0) { # if several models are provided
+    modNames <- all.vars(match.call()) # store the names of the models given as arguments
+
+    models[[length(models)+1]] <- object
+    AIC <- rep(NA,length(models))
+
+    for(i in 1:length(models)) {
+      m <- models[[i]]
+      nbPar <- length(m$mle$stepPar)+length(m$mle$anglePar)+length(m$mle$beta)+length(m$mle$delta)-1
+      maxLogLike <- -m$mod$minimum
+      AIC[i] <- -2*maxLogLike+k*nbPar
+    }
+
+    return(data.frame(Model=modNames,AIC=AIC))
+  }
+  else { # if only one model is provided
+    m <- object
+    nbPar <- length(m$mle$stepPar)+length(m$mle$anglePar)+length(m$mle$beta)+length(m$mle$delta)-1
+    maxLogLike <- -m$mod$minimum
+    AIC <- -2*maxLogLike+k*nbPar
+
+    return(AIC)
+  }
 }
