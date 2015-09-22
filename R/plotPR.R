6a40d18d6134ed21d5be303e258deccafc01d76f
TheoMichelot <theo.michelot@gmail.com>
2015-09-22 13:19:51 +0100

    plotPR function

---
+++ R/plotPR.R
@@ -0,0 +1,38 @@
+
+#' Plot pseudo-residuals
+#'
+#' Plots qq-plots and time series of the pseudo-residuals
+#'
+#' @param pr Pseudo-residuals (as output by \code{\link{pseudoRes}})
+#'
+#' @examples
+#' m <- ex$m # moveHMM object (as returned by \code{\link{fitHMM}})
+#'
+#' pr <- pseudoRes(m)
+#' plotPR(pr)
+
+plotPR <- function(pr)
+{
+  par(mfrow=c(2,2))
+
+  # steps qq-plot
+  qqStep <- qqnorm(pr$stepRes,plot=FALSE)
+  limInf <- min(min(qqStep$x,na.rm=T),min(qqStep$y,na.rm=T))
+  limSup <- max(max(qqStep$x,na.rm=T),max(qqStep$y,na.rm=T))
+  qqnorm(pr$stepRes,main="Steps pseudo-residuals",col="red",xlim=c(limInf,limSup),ylim=c(limInf,limSup))
+  abline(0,1,lwd=2)
+
+  # angles qq-plot
+  qqAngle <- qqnorm(pr$angleRes,plot=FALSE)
+  limInf <- min(min(qqAngle$x,na.rm=T),min(qqAngle$y,na.rm=T))
+  limSup <- max(max(qqAngle$x,na.rm=T),max(qqAngle$y,na.rm=T))
+  qqnorm(pr$angleRes,main="Angles pseudo-residuals",col="red",xlim=c(limInf,limSup),ylim=c(limInf,limSup))
+  abline(0,1,lwd=2)
+
+  # time series
+  plot(pr$stepRes,type="l",xlab="Observation index",ylab="Steps pseudo-residuals")
+  plot(pr$angleRes,type="l",xlab="Observation index",ylab="Angles pseudo-residuals")
+
+  # back to default
+  par(mfrow=c(1,1))
+}
