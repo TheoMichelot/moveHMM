70a3742291c67274ef9d31d95d74a2e1adfaab00
TheoMichelot <theo.michelot@gmail.com>
2015-10-18 15:52:43 +0100

    adapt test_CI to new CI format

--- tests/testthat/test_CI.R
+++ tests/testthat/test_CI.R
@@ -5,11 +5,11 @@ test_that("Output has the right format",{
   m <- ex$m
   c <- CI(m)
 
-  expect_equal(length(c),2)
-  expect_equal(dim(c$lower$stepPar),dim(m$mle$stepPar))
-  expect_equal(dim(c$upper$stepPar),dim(m$mle$stepPar))
-  expect_equal(dim(c$lower$anglePar),dim(m$mle$anglePar))
-  expect_equal(dim(c$upper$anglePar),dim(m$mle$anglePar))
-  expect_equal(dim(c$lower$beta),dim(m$mle$beta))
-  expect_equal(dim(c$upper$beta),dim(m$mle$beta))
+  expect_equal(length(c),3)
+  expect_equal(dim(c$stepPar$lower),dim(m$mle$stepPar))
+  expect_equal(dim(c$stepPar$upper),dim(m$mle$stepPar))
+  expect_equal(dim(c$anglePar$lower),dim(m$mle$anglePar))
+  expect_equal(dim(c$anglePar$upper),dim(m$mle$anglePar))
+  expect_equal(dim(c$beta$lower),dim(m$mle$beta))
+  expect_equal(dim(c$beta$upper),dim(m$mle$beta))
 })
