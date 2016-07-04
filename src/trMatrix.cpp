6510af6684b2351be810a0e0a8393c9072564ced
TheoMichelot <theo.michelot@gmail.com>
2016-07-04 16:14:44 +0100

    indent = 2 to indent = 4

--- src/trMatrix.cpp
+++ src/trMatrix.cpp
@@ -18,34 +18,34 @@ using namespace std;
 // [[Rcpp::export]]
 arma::cube trMatrix_rcpp(int nbStates, arma::mat beta, arma::mat covs)
 {
-  int nbObs = covs.n_rows;
-  arma::cube trMat(nbStates,nbStates,nbObs);
-  trMat.zeros();
-  arma::mat rowSums(nbStates,nbObs);
-  rowSums.zeros();
+    int nbObs = covs.n_rows;
+    arma::cube trMat(nbStates,nbStates,nbObs);
+    trMat.zeros();
+    arma::mat rowSums(nbStates,nbObs);
+    rowSums.zeros();
 
-  arma::mat g(nbObs,nbStates*(nbStates-1));
-  g = covs*beta;
+    arma::mat g(nbObs,nbStates*(nbStates-1));
+    g = covs*beta;
 
-  for(int k=0;k<nbObs;k++) {
-    int cpt=0;
-    for(int i=0;i<nbStates;i++) {
-      for(int j=0;j<nbStates;j++) {
-        if(i==j) {
-          trMat(i,j,k)=1;
-          cpt++;
+    for(int k=0;k<nbObs;k++) {
+        int cpt=0;
+        for(int i=0;i<nbStates;i++) {
+            for(int j=0;j<nbStates;j++) {
+                if(i==j) {
+                    trMat(i,j,k)=1;
+                    cpt++;
+                }
+                else trMat(i,j,k) = exp(g(k,i*nbStates+j-cpt));
+                rowSums(i,k)=rowSums(i,k)+trMat(i,j,k);
+            }
         }
-        else trMat(i,j,k) = exp(g(k,i*nbStates+j-cpt));
-        rowSums(i,k)=rowSums(i,k)+trMat(i,j,k);
-      }
     }
-  }
 
-  // normalization
-  for(int k=0;k<nbObs;k++)
-    for(int i=0;i<nbStates;i++)
-      for(int j=0;j<nbStates;j++)
-        trMat(i,j,k) = trMat(i,j,k)/rowSums(i,k);
+    // normalization
+    for(int k=0;k<nbObs;k++)
+        for(int i=0;i<nbStates;i++)
+            for(int j=0;j<nbStates;j++)
+                trMat(i,j,k) = trMat(i,j,k)/rowSums(i,k);
 
-  return trMat;
+    return trMat;
 }
