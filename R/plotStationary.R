546a89c66c81dbecfb91bcb62044817ecdde8048
Théo Michelot <tm75@st-andrews.ac.uk>
2022-05-09 16:46:21 +0100

    use getPlotData in plotStationary

--- R/plotStationary.R
+++ R/plotStationary.R
@@ -14,49 +14,34 @@
 #' plotStationary(m)
 #'
 #' @export
-plotStationary <- function(m, col=NULL, plotCI=FALSE, alpha=0.95)
-{
-    if(!is.moveHMM(m))
+plotStationary <- function(m, col=NULL, plotCI=FALSE, alpha=0.95) {
+    # check/unpack arguments
+    if(!is.moveHMM(m)) {
         stop("'m' must be a moveHMM object (as output by fitHMM)")
-
+    }
     data <- m$data
     nbStates <- ncol(m$mle$stepPar)
     beta <- m$mle$beta
-
-    if(nrow(beta)==1)
+    if(nrow(beta)==1) {
         stop("No covariate effect to plot (nrow(beta)==1).")
+    }
 
     # prepare colors for the states
     if(is.null(col) | (!is.null(col) & length(col) != nbStates)) {
         col <- getPalette(nbStates = nbStates)
     }
 
-    rawCovs <- m$rawCovs
-    gridLength <- 100
+    plotData <- getPlotData(m = m, type = "stat", format = "wide")
 
     # loop over covariates
-    for(cov in 1:ncol(rawCovs)) {
-        inf <- min(rawCovs[,cov], na.rm = TRUE)
-        sup <- max(rawCovs[,cov], na.rm = TRUE)
-
-        # mean values of each covariate
-        meanCovs <- colMeans(rawCovs)
-
-        # set all covariates to their mean, except for "cov"
-        # (which takes a grid of values from inf to sup)
-        tempCovs <- as.data.frame(matrix(rep(meanCovs, each = gridLength),
-                                         nrow = gridLength))
-        tempCovs[,cov] <- seq(inf, sup, length = gridLength)
-        colnames(tempCovs) <- colnames(rawCovs)
-
-        stat <- predictStationary(m = m, newData = tempCovs,
-                                   returnCI = TRUE, alpha = 0.95)
-
+    for(cov in 1:length(plotData)) {
+        covGrid <- plotData[[cov]][,1]
         # plot lines
-        plot(NA, xlim = c(inf, sup), ylim = c(0, 1), xlab = names(rawCovs)[cov],
+        plot(NA, xlim = range(covGrid), ylim = c(0, 1), xlab = names(plotData)[cov],
              ylab = "Stationary state probabilities")
         for(state in 1:nbStates) {
-            lines(tempCovs[,cov], stat$mle[,state], col = col[state], lwd = 2)
+            lines(covGrid, plotData[[cov]][,paste0("S", state)],
+                  col = col[state], lwd = 2)
         }
         legend("topleft", legend = paste("state", 1:nbStates), col = col,
                lty = 1, bty = "n")
@@ -65,9 +50,8 @@ plotStationary <- function(m, col=NULL, plotCI=FALSE, alpha=0.95)
         if(plotCI) {
             for(state in 1:nbStates) {
                 options(warn = -1) # to muffle "zero-length arrow..." warning
-                # plot the confidence intervals
-                arrows(tempCovs[,cov], stat$lci[,state],
-                       tempCovs[,cov], stat$uci[,state],
+                arrows(covGrid, plotData[[cov]][,paste0("S", state, ".lci")],
+                       covGrid, plotData[[cov]][,paste0("S", state, ".uci")],
                        length = 0.025, angle = 90, code = 3,
                        col = col[state], lwd = 0.7)
                 options(warn = 1)
